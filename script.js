43dab271c7877b5e26050a94215a50c82750d29b
JustANipple <prandisamuel99@gmail.com>
2023-03-02 11:54:41 +0100

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const socialBox = document.querySelector(".share");
 const avatarBox = document.querySelector(".avatar");
 
 const rootStyles = window.getComputedStyle(document.documentElement);
-const shareBtnBgClr = rootStyles.getPropertyValue("")
+const shareClr = rootStyles.getPropertyValue("--par-clr");
+const avatarClr = rootStyles.getPropertyValue("--bg-clr");
 
 shareBtn.addEventListener("click", showOrHide);
 
@@ -11,9 +12,12 @@ function showOrHide() {
     if(socialBox.style.display === "none") {
         socialBox.style.display = "block";
         avatarBox.style.display = "none";
-        shareBtn.style.backgroundColor = ""
+        shareBtn.style.backgroundColor = shareClr;
+        shareBtn.children[0].style.filter = "brightness(0) invert(1)";
     } else {
         socialBox.style.display = "none";
         avatarBox.style.display = "flex";
+        shareBtn.style.backgroundColor = avatarClr;
+        shareBtn.children[0].style.filter = "unset";
     }
 }
